dd6e3198078e986eb10c95612627ae4048f7c5ca
Matvei <matvesch@protonmail.com>
2022-01-30 21:13:27 +0400

    board displayed

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,14 @@
 const displayBoard = (board) => {
 	const gameBoardContainer = document.getElementById("game-board");
+	gameBoardContainer.classList.add('board');
 
 	for (let i = 0; i < board.length; i++) {
-		const oneCell = document.createElement('div');
-		oneCell.classList.add('cell');
-		oneCell.id = i;
-		gameBoardContainer.appendChild(oneCell);
+		for (let j = 0; j < board[i].length; j++) {
+			const oneCell = document.createElement('div');
+			oneCell.classList.add('cell');
+			oneCell.id = i;
+			gameBoardContainer.appendChild(oneCell);
+		}
 	}
 };
 
