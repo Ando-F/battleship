d0d12f6e14206e30835d4c07c06898c7c27df904
Matvei <matvesch@protonmail.com>
2022-01-31 15:16:48 +0400

    hover event added

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,23 @@
-const displayBoard = (board, container) => {
+const displayBoard = (board, container, uniqueClass) => {
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < board[i].length; j++) {
 			const oneCell = document.createElement('div');
 			oneCell.classList.add('cell');
-			oneCell.id = i;
+			oneCell.classList.add(uniqueClass);
+			oneCell.innerHTML = board[i][j];
+			oneCell.id = (i * 10) + j;
 			container.appendChild(oneCell);
 		}
 	}
 };
 
+const changeColorOnHover = (element, listener, color) => {
+	element.addEventListener(listener, () => {
+		element.style.background = color;
+	});
+};
+
 export {
-	displayBoard
+	displayBoard,
+	changeColorOnHover
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import {Gameboard} from "./factories";
-import {displayBoard} from "./dom";
+import {changeColorOnHover, displayBoard} from "./dom";
 import './style.css';
 
+//display player's board
 const gameBoardContainer = document.getElementById("game-board");
 const firstGameBoard = Gameboard();
-displayBoard(firstGameBoard.board, gameBoardContainer);
+displayBoard(firstGameBoard.board, gameBoardContainer, 'my-board');
 
+//display enemy's board
 const enemygameBoardContainer = document.getElementById("enemy-game-board");
 const enemyBoard = Gameboard();
-displayBoard(enemyBoard.board, enemygameBoardContainer);
+displayBoard(enemyBoard.board, enemygameBoardContainer, 'enemy-board');
+
+const gameBoardCells = document.querySelectorAll('.my-board');
+gameBoardCells.forEach((cell) => {
+	changeColorOnHover(cell, 'mouseover', 'yellow');
+	changeColorOnHover(cell, 'mouseout', 'white');
+})
