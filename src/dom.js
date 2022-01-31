5dd532280c8e5ba6cb7ec597b243e39ba848c36f
Matvei <matvesch@protonmail.com>
2022-01-31 10:38:32 +0400

    boards are printed

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,10 @@
-const displayBoard = (board) => {
-	const gameBoardContainer = document.getElementById("game-board");
-	gameBoardContainer.classList.add('board');
-
+const displayBoard = (board, container) => {
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < board[i].length; j++) {
 			const oneCell = document.createElement('div');
 			oneCell.classList.add('cell');
 			oneCell.id = i;
-			gameBoardContainer.appendChild(oneCell);
+			container.appendChild(oneCell);
 		}
 	}
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import {Gameboard} from "./factories";
 import {displayBoard} from "./dom";
 import './style.css';
 
-console.log('hello');
-
-const body = document.querySelector('body');
-const h1 = document.createElement('h1');
-h1.innerHTML = 'Hello';
-h1.classList.add('hello');
-body.appendChild(h1);
-
+const gameBoardContainer = document.getElementById("game-board");
 const firstGameBoard = Gameboard();
-displayBoard(firstGameBoard.board)
+displayBoard(firstGameBoard.board, gameBoardContainer);
+
+const enemygameBoardContainer = document.getElementById("enemy-game-board");
+const enemyBoard = Gameboard();
+displayBoard(enemyBoard.board, enemygameBoardContainer);
