2d308f12c1d64c880ea81c63367b2a8e713df93b
Matvei <matvesch@protonmail.com>
2022-02-07 22:10:31 +0400

    now I have function that checks the board

--- src/Player.js
+++ src/Player.js
@@ -2,34 +2,29 @@ const Player = () => {
 	let xArray;
 	let yArray;
 
+	let numbersX = [];
+	let numbersY = [];
+
+	const createArray = (length, axis) => {
+		if (axis === 'x') {
+			xArray = Array.from(Array(10 - length).keys());
+			yArray = Array.from(Array(10).keys());
+		} else if (axis === 'y') {
+			xArray = Array.from(Array(10).keys());
+			yArray = Array.from(Array(10 - length).keys());
+		}
+	}
+
 	const randomCoordinates = (length, axis) => {
 		if (length === 1) {
 			xArray = Array.from(Array(10).keys());
 			yArray = Array.from(Array(10).keys());
 		} else if (length === 2) {
-			if (axis === 'x') {
-				xArray = Array.from(Array(8).keys());
-				yArray = Array.from(Array(10).keys());
-			} else if (axis === 'y') {
-				xArray = Array.from(Array(10).keys());
-				yArray = Array.from(Array(8).keys());
-			}
+			createArray(2, axis);
 		} else if (length === 3) {
-			if (axis === 'x') {
-				xArray = Array.from(Array(7).keys());
-				yArray = Array.from(Array(10).keys());
-			} else if (axis === 'y') {
-				xArray = Array.from(Array(10).keys());
-				yArray = Array.from(Array(7).keys());
-			}
+			createArray(3, axis);
 		} else if (length === 4) {
-			if (axis === 'x') {
-				xArray = Array.from(Array(6).keys());
-				yArray = Array.from(Array(10).keys());
-			} else if (axis === 'y') {
-				xArray = Array.from(Array(10).keys());
-				yArray = Array.from(Array(6).keys());
-			}
+			createArray(4, axis);
 		}
 
 		let x = xArray[Math.floor(Math.random()*xArray.length)];
@@ -43,6 +38,17 @@ const Player = () => {
 		return {x: x, y: y};
 	};
 
+	const checkBoard = (board) => {
+		for (let i = 0; i < board.length; i++) {
+			for (let j = 0; j < board[i].length; j++) {
+				if (board[i][j] === 1) {
+					numbersX.push(i);
+					numbersY.push(j);
+				}
+			}
+		}
+	}
+
 	const randomAxis = () => {
 		let randomNumber = Math.floor(Math.random() * (2 - 1 + 1) + 1);
 
@@ -53,7 +59,7 @@ const Player = () => {
 		}
 	}
 
-	return {randomAxis, randomCoordinates, xArray, yArray};
+	return {randomAxis, randomCoordinates, xArray, yArray, checkBoard, numbersY, numbersX};
 }
 
 export {
--- tests/player.test.js
+++ tests/player.test.js
@@ -7,3 +7,19 @@ test('place ship with random coordinates', () => {
 	gameBoard.placeShip(1, {value: 'x'}, computer.randomCoordinates(1, 'x'));
 	expect(gameBoard.ships.length).toBe(1);
 });
+
+test('can we check gameboard', () => {
+	const gameboard = Gameboard();
+	const computer = Player();
+	gameboard.placeShip(2, {value: 'x'}, {x: 1, y: 2});
+	computer.checkBoard(gameboard.board);
+	expect(computer.numbersX[1]).toBe(2);
+})
+
+test('can we check gameboard – numbersY have the all same numbers', () => {
+	const gameboard = Gameboard();
+	const computer = Player();
+	gameboard.placeShip(2, {value: 'x'}, {x: 1, y: 2});
+	computer.checkBoard(gameboard.board);
+	expect(computer.numbersY[1]).toBe(2);
+})
